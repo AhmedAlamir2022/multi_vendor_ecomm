05ffbaf4c6c16ceba75944403cb24ef6b178f689
Ahmed Alamir <ahmedalamir521@gmail.com>
2024-12-27 17:53:44 +0300

    vendor_products_crud

--- resources/views/vendor/layouts/sidebar.blade.php
+++ resources/views/vendor/layouts/sidebar.blade.php
@@ -5,22 +5,12 @@
     </span>
     <a href="javascript:;" class="dash_logo"><img src="" alt="logo" class="img-fluid"></a>
     <ul class="dashboard_link">
-        <li><a href=""><i class="fas fa-tachometer"></i>Dashboard</a></li>
-        <li><a href=""><i class="fas fa-tachometer"></i>Messages</a></li>
-        <li><a class="" href="{{ url('/') }}"><i class="fas fa-home"></i>Go To Home Page</a></li>
-        <li><a href=""><i class="fas fa-list-ul"></i> Orders</a></li>
-        <li><a href=""><i class="far fa-star"></i> Reviews</a></li>
+        <li><a class="{{setActive(['vendor.products.*'])}}" href="{{route('vendor.products.index')}}"><i class="fas fa-cart-plus"></i> Products</a></li>
 
         <li><a class="{{ setActive(['vendor.shop-profile.index']) }}" href="{{ route('vendor.shop-profile.index') }}"><i class="far fa-user"></i> Shop Profile</a></li>
         <li><a href="{{ route('vendor.profile') }}"><i class="far fa-user"></i> My Profile</a></li>
-        
-        <li><a href=""><i class="fal fa-gift-card"></i> Addresses</a></li>
-        @if (auth()->user()->role !== 'vendor')
-            <li><a href=""><i class="far fa-user"></i> Request to be
-                    vendor</a></li>
-        @endif
-        <li>
 
+        <li>
             <form method="POST" action="{{ route('logout') }}">
                 @csrf
                 <a href="{{ route('logout') }}"
