d2806298bfe7aba3d3793c805bac013d87c006c8
Ahmed Alamir <ahmedalamir521@gmail.com>
2024-12-28 11:15:07 +0300

    admin_shipping_rule_crud

--- routes/admin.php
+++ routes/admin.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\Backend\ProductVariantItemController;
 use App\Http\Controllers\Backend\ProfileController;
 use App\Http\Controllers\Backend\SellerProductController;
 use App\Http\Controllers\Backend\SettingController;
+use App\Http\Controllers\Backend\ShippingRuleController;
 use App\Http\Controllers\Backend\SliderController;
 use App\Http\Controllers\Backend\SubCategoryController;
 
@@ -110,6 +111,10 @@ Route::put('generale-setting-update', [SettingController::class, 'generalSetting
 /** Coupon Routes */
 Route::put('coupons/change-status', [CouponController::class, 'changeStatus'])->name('coupons.change-status');
 Route::resource('coupons', CouponController::class);
+
+/** Shipping Rule Routes */
+Route::put('shipping-rule/change-status', [ShippingRuleController::class, 'changeStatus'])->name('shipping-rule.change-status');
+Route::resource('shipping-rule', ShippingRuleController::class);
 
 
 
